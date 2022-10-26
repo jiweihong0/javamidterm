27345a2913ff0555d525d6044b1340fef34847bd
jiweihong0 <65114048+jiweihong0@users.noreply.github.com>
2022-10-26 16:22:58 +0800

    feat:練習結果

---
+++ src/App65.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App65 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // 初始化 Scanner 物件
+        System.out.print("請輸入A的好友:");
+        String[] A = scanner.nextLine().split(" ");
+        System.out.print("請輸入B的好友:");
+        String[] B = scanner.nextLine().split(" ");
+        int mutfri=0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i].equals(B[j])) {
+                    mutfri++;
+                }
+            }
+        }
+        System.out.println(mutfri);
+        
+    }
+}
